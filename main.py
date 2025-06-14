d85a40f4dcefa1cd7383b307e3bddabd3ce21971
personalprojects <personalprojects@192.168.1.24>
2025-06-14 13:12:36 +0100

    final product

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
 
 # Sets up the properties of our screen
 screen = Screen()
@@ -10,6 +13,12 @@ screen.title("My Snake Game")
 
 # create a snake
 snake = Snake()
+
+# create the food
+food = Food()
+
+# create scoreboard
+scoreboard = Scoreboard()
 
 #Control the snake
 screen.listen() # Listening
@@ -25,5 +34,26 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        # updates the needed parameters namely:
+        food.refresh() # position of the food
+        snake.extend() # length of the snake
+        scoreboard.increase_score() # scoreboard
+
+    # Detect collision with wall
+    if snake.head.xcor() < -296 or snake.head.xcor() > 296 or snake.head.ycor() < -296 or snake.head.ycor() > 270:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    # if the head collides with any segment in the tail:
+        # trigger game_over
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -16,11 +16,31 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:  # sets up the body of the snake
-            new_segment = Turtle(shape='square')
-            new_segment.color('white')
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+
+    def add_segment(self, position): # creates a new segment and places it at 'position'
+        new_segment = Turtle(shape='square')
+        new_segment.color('white')
+        new_segment.penup() # not write the path of the snake on the screen
+        new_segment.goto(position) # fixes the bug: the square shows at the center at each food-snake collision
+        self.segments.append(new_segment) # append it to the list of segments
+
+    def extend(self): # adds a new segment to the snake
+        # add_segment is triggered with the argument position of the last segment of the snake
+        self.add_segment(self.segments[-1].position())
+
+
+    def move(self):
+        # Each segment of the snake takes the place of the segment in front of it starting from the end of the snake and
+        # excluding the head of the snake. This approach makes it easier to turn the snake towards different directions by
+        # only modifying the direction of the head.
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # seg_num ranges from the length of the segment minus 1 until 1 by one unit decrement step
+            x_cor = self.segments[seg_num - 1].xcor()  # storing the x coordinate of the front snake segment in this variable
+            y_cor = self.segments[seg_num - 1].ycor()  # storing the y coordinate of the front snake segment in this variable
+            self.segments[seg_num].goto(x=x_cor, y=y_cor)  # take the targeted segment to the new position
+        self.head.forward(MOVE_DISTANCE)  # Moves the head 20 pixels to the front
 
     # Controls the snake
     def up(self):
@@ -38,17 +58,6 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT) # sets the heading towards the east
-
-    def move(self):
-        # Each segment of the snake takes the place of the segment in front of it starting from the end of the snake and
-        # excluding the head of the snake. This approach makes it easier to turn the snake towards different directions by
-        # only modifying the direction of the head.
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            # seg_num ranges from the length of the segment minus 1 until 1 by one unit decrement step
-            x_cor = self.segments[seg_num - 1].xcor()  # storing the x coordinate of the front snake segment in this variable
-            y_cor = self.segments[seg_num - 1].ycor()  # storing the y coordinate of the front snake segment in this variable
-            self.segments[seg_num].goto(x=x_cor, y=y_cor)  # take the targeted segment to the new position
-        self.head.forward(MOVE_DISTANCE)  # Moves the head 20 pixels to the front
 
 
 
